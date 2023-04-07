0fbbe348c3b3415262e679b33523064fe47dfd85
kingfariz <52910517+kingfariz@users.noreply.github.com>
2023-04-07 14:12:58 +0700

    Added new themes

--- lib/helpers/themes.dart
+++ lib/helpers/themes.dart
@@ -5,6 +5,9 @@ const Color primaryColor = Color(0xff2C96F1);
 const Color whiteColor = Color(0xffffffff);
 const Color redColor = Color(0xffFF0000);
 const Color primaryTextColor = Color(0xff1b1b1c);
+const Color softGreyColor = Color(0xFFaaaaaa);
+const Color greyColor = Color(0xFF515151);
+const Color lightgreyColor = Color(0xff777777);
 
 //Margins
 const double defaultMargin = 20.0;
@@ -15,3 +18,19 @@ TextStyle primaryTextStyle =
 
 TextStyle primaryBigTextStyle =
     const TextStyle(color: primaryTextColor, fontSize: 22);
+
+TextStyle sectionTitle = const TextStyle(
+    color: primaryTextColor, fontSize: 16, fontWeight: FontWeight.bold);
+
+TextStyle productTotalReview = const TextStyle(
+    color: softGreyColor, fontSize: 11, fontWeight: FontWeight.bold);
+
+TextStyle productTotalReviewLight =
+    const TextStyle(color: softGreyColor, fontSize: 11);
+
+TextStyle productName = const TextStyle(color: greyColor, fontSize: 12);
+
+TextStyle productPrice = const TextStyle(
+    color: primaryTextColor, fontSize: 13, fontWeight: FontWeight.bold);
+
+TextStyle productSale = const TextStyle(color: softGreyColor, fontSize: 11);
