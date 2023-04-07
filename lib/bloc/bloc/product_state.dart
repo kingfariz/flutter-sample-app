fa5dc17565b4bfac3bf322262e0b976ba72a833f
kingfariz <52910517+kingfariz@users.noreply.github.com>
2023-04-07 17:06:54 +0700

    removed immutable

---
+++ lib/bloc/bloc/product_state.dart
@@ -0,0 +1,25 @@
+part of 'product_bloc.dart';
+
+abstract class CrudState {}
+
+class CrudLoading extends CrudState {}
+
+class CrudError extends CrudState {}
+
+class ReadDataSuccess extends CrudState {
+  final List<dynamic> data;
+  ReadDataSuccess(this.data);
+  List<Object?> get props => [data];
+}
+
+class SendDataSuccess extends CrudState {}
+
+class UpdateDataSuccess extends CrudState {}
+
+class DeleteDataSuccess extends CrudState {}
+
+class GetDataSuccess extends CrudState {
+  final Map<String, dynamic> data;
+  GetDataSuccess(this.data);
+  List<Object?> get props => [data];
+}
